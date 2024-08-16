69046076fb103d74e6e0249c7613f80c2a7b36f8
Cesar Mejia <rasec8me@gmail.com>
2024-08-16 15:01:23 -0600

    add TodosDatabaseImp

---
+++ CleanAsyncObservation/Framework/Local/TodosDatabaseImp.swift
@@ -0,0 +1,60 @@
+//
+//  TodosDatabaseImp.swift
+//  CleanAsyncObservation
+//
+//  Created by Ces Mejia on 16/08/24.
+//
+
+import Foundation
+import OSLog
+
+private let logger = Logger(subsystem: "Framework Local", category: "Todos Database")
+
+class TodosDatabaseImp: TodosDatabase {
+    var directoryURL: URL?
+    
+    init(directoryURL: URL? = nil) {
+        self.directoryURL = directoryURL
+    }
+    
+    func queryTodos() async throws(LocalDataSourceError) -> [Todo] {
+        do {
+            let todos = try await loadTodos()
+            logger.info("Succesfully queried TODOs from database: \(todos.count)")
+            return todos
+        } catch {
+            logger.error("Error querying TODOs: \(error)")
+            throw .unknown
+        }
+    }
+    
+    func updateTodos(with todos: [Todo]) async throws(LocalDataSourceError) {
+        do {
+            try await save(todos: todos)
+            logger.info("Succesfully updated TODOs: \(todos.count)")
+        } catch {
+            logger.error("Error updating TODOs: \(error)")
+            throw .unknown
+        }
+    }
+    
+    // MARK: - FileManager
+    
+    private func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("todos.data")
+    }
+    
+    private func loadTodos() async throws -> [Todo] {
+        let fileURL = try directoryURL ?? fileURL()
+        guard let data = try? Data(contentsOf: fileURL) else { return [] }
+        let todos = try JSONDecoder().decode([Todo].self, from: data)
+        return todos
+    }
+    
+    private func save(todos: [Todo]) async throws {
+        let data = try JSONEncoder().encode(todos)
+        let outfile = try directoryURL ?? fileURL()
+        try data.write(to: outfile)
+    }
+}
